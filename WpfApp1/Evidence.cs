60e18671135363c81fa239fef9597d9cccf827dc
m1k4los <22ib19_kunt@U1-14>
2024-04-23 08:24:06 +0200

    Přidejte soubory projektu.

---
+++ WpfApp1/Evidence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Evidence
+    {
+        private List<Polozka> Polozky;
+
+        public Evidence()
+        {
+            Polozky = new List<Polozka>();
+        }
+
+        public void PridatPolozku(Polozka polozka)
+        {
+            Polozky.Add(polozka);
+        }
+    }
+}
---
+++ WpfApp1/Polozka.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Polozka
+    {
+        private string Nazev;
+        private int Cena;
+        private int Id;
+
+        public Polozka(string nazev, int cena, int id)
+        {
+            Nazev = nazev;
+            Cena = cena;
+            Id = id;
+        }
+    }
+}
